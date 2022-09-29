e438f90fc57c6cb04aafdff8a1668d5ed498c057
Juan Estrada <100156819+david24748@users.noreply.github.com>
2022-09-28 22:16:04 -0500

    week3

--- js/main.js
+++ js/main.js
@@ -2,8 +2,10 @@ const links = [
     {
       label: "Week1 notes",
       url: "Week1/index.html",
-      label2: "week2 notes",
-      url2: "Week2/index.html"
+      label2: "Week2 notes",
+      url2: "Week2/index.html",
+      label3: "Week3 notes",
+      url3: "Week3/index.html"
     }
   ]
 
@@ -41,3 +43,20 @@ function loadIndextwo() {
   })
 }
 loadIndextwo()
+
+function loadIndexthree() {
+  const ol = document.querySelector("#linkslistthree");
+
+  links.forEach(link => {
+      const li = document.createElement("li");
+      const href = document.createElement("a");
+      href.setAttribute("href", link.url3);
+      href.innerText= link.label3;
+
+      li.appendChild(href);
+      ol.appendChild(li);
+
+      
+  })
+}
+loadIndexthree()
